63166e4c572ee79251f1003c238fdab2e1174180
Admiral404 <115575654+X404Studio@users.noreply.github.com>
2026-07-02 10:49:52 +0700

    Import useState and useEffect in SummaryPage

--- src/pages/SummaryPage.jsx
+++ src/pages/SummaryPage.jsx
@@ -1,5 +1,5 @@
+import { useState, useEffect } from "react";
 import { Layout, Card } from "antd";
-
 import {
   BarChart,
   Bar,
@@ -10,7 +10,6 @@ import {
   ResponsiveContainer,
   PieChart,
   Pie,
-  
 } from "recharts";
 import Sidebar from "../components/Sidebar";
 
